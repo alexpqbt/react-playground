c47e5f129f84629b9b8469937caf6ac9e6782725
Alex Paquibot <115098095+alexpqbt@users.noreply.github.com>
2025-12-26 12:04:01 +0800

    Format quickstart

--- src/playground/quickstart/QuickStart.jsx
+++ src/playground/quickstart/QuickStart.jsx
@@ -1,14 +1,16 @@
 function MyButton() {
   return (
-    <button className="bg-gray-200 outline-1 outline-solid">This is a boring ass button</button>
+    <button className="bg-gray-200 outline-1 outline-solid">
+      This is a boring ass button
+    </button>
   );
 }
 
 export default function QuickStart() {
   return (
-    <div className="outline-solid p-1">
+    <div className="component">
       <h1>Welcome to quickstart!</h1>
       <MyButton />
     </div>
-  )
+  );
 }
